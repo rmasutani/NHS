5510e57557a2ebf1b9d535e1d74aa5fb5dbdd0e7
rmasutani <masutanirugby@gmail.com>
2021-05-26 01:11:59 +0900

    Add bot codes

---
+++ workspace/ch3/hubot-todo/.bash_history
@@ -0,0 +1,14 @@
+yo hubot-yarn --adapter=slack
+touch scripts/todo-bot.js
+yarn add ./todo
+ls
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+env HUBOT_SLACK_TOKEN=xoxb-2096833041491-2096848119459-o4rMNzT8KodoizTi4I0N0lox bin/hubot --adapter slack 
+env HUBOT_SLACK_TOKEN=xoxb-2096833041491-2096848119459-CDGNnyZYC1aUVjyVzLRdJGQc bin/hubot --adapter slack 
+env HUBOT_SLACK_TOKEN=xoxb-2096833041491-2096848119459-CDGNnyZYC1aUVjyVzLRdJGQc bin/hubot --adapter slack 
+env HUBOT_SLACK_TOKEN=xoxb-2096833041491-2096848119459-CDGNnyZYC1aUVjyVzLRdJGQc bin/hubot --adapter slack 
+exit
---
+++ workspace/ch3/hubot-todo/scripts/todo-bot.js
@@ -0,0 +1,38 @@
+// Description:
+//   TODO を管理できるボットです
+// Commands:
+//   ボット名 add      - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
+"use strict";
+const todo = require("todo");
+
+module.exports = (robot) => {
+  robot.respond(/add (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.add(task);
+    msg.send("Added: " + task);
+  });
+  robot.respond(/done (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.add(task);
+    msg.send("Completed: " + task);
+  });
+  robot.respond(/del (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.add(task);
+    msg.send("Deleted: " + task);
+  });
+  robot.respond(/list/i, (msg) => {
+    if (!todo.list().length) {
+      msg.send("There are no todos now.");
+    } else {
+      msg.send(todo.list().join("\n"));
+    }
+  });
+  robot.respond(/donelist/i, (msg) => {
+    msg.send(todo.donelist().join("\n"));
+  });
+};
